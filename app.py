0833a0faa310650f23bcd3a98258af1bb92e4732
DwanW <kunvang@yahoo.com>
2020-06-17 14:17:52 -0600

    1

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_restful import Api
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def helloWorld():
-  return "Hello, cross-origin-world!"
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
+api = Api(app)
+
+
+
+# api.add_resource(Item, '/item/<string:name>')
+
+# @app.route("/")
+# def helloWorld():
+#   return "Hello, cross-origin-world!"
+
+if __name__ == '__main__':
+    app.run(debug=True)  # important to mention debug=True
