f6a4c10da7484f876c5441a33cc6b246b9677e7d
DwanW <kunvang@yahoo.com>
2020-06-30 15:26:30 -0600

    added comment for the util function

--- client/src/util.js
+++ client/src/util.js
@@ -2,6 +2,7 @@ function getMarkersFromList(list, allLocationList){
     if(list.length === 0){
         return [];
     }
+    // filters(array.filter) location where product list of the location contains(array.includes) at least one(array.some) name from the list of names checked 
     let filteredLocationList = allLocationList.filter((location) => list.some((e)=> location["products"].map((e)=>e.name).includes(e)));
     return filteredLocationList;
 }
